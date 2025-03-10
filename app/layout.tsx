b3171f61e99110874c2aa0c072ad3a0159d00902
Shubham Pandey <69081168+heizshubham@users.noreply.github.com>
2025-03-10 16:02:36 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'shubham portfolio',
+  description: 'Created by shubham with typescript',
 }
 
 export default function RootLayout({
